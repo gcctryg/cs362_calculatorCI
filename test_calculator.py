5946cd2c17ea4451f7ab841078ff4b4dc8b18348
Xuelu Feng <gcctryg@gmail.com>
2021-06-09 14:18:18 -0700

    added test

---
+++ test_calculator.py
@@ -0,0 +1,9 @@
+import calculator
+
+class testCalculatorApp:
+    def test_add(self):
+        assert 5 == calculator.add(2,3)
+
+    def test_subtract(self):
+        assert 5 == calculator.add(10,5)
+        
